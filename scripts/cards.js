481a7fb1bfe59e3491a209961825fae90fd120ce
darchitetto <denise@digital-pioneers.com>
2016-03-31 16:24:16 -0400

    introduced index and script files, no functionality yet

---
+++ scripts/cards.js
@@ -0,0 +1,23 @@
+const rank = {cards: [
+    {Ace: "A"},
+    {King: "K"},
+    {Queen: "Q"},
+    {Jack : "J"},
+    {10 : "10"},
+    {9 : "9"},
+    {8 : "8"},
+    {7 : "7"},
+    {6 : "6"},
+    {5 : "5"},
+    {4 : "4"},
+    {3 : "3"},
+    {2 : "2"}
+]};
+
+
+const suit = {suit: [
+    {hearts:"hearts"},
+    {spades:"spades"},
+    {diamonds:"diamonds"},
+    {clubs: "clubs"}
+]};
