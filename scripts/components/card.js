9dfca6ca13f73e44ae1c2a74bbdcc2fc4df49843
darchitetto <denise@digital-pioneers.com>
2016-04-03 08:20:50 -0400

    Initial Card Component

---
+++ scripts/components/card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class card extends React.component{
+
+    constructor(){
+
+    };
+
+    render(){
+        return (
+            <div> My Card </div>
+        )
+    };
+}
